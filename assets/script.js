ce9ebe23c5cc8dd5ad6004f6b6aa0be578e278a9
Judit <hello@juditdidit.com>
2015-03-12 14:47:46 -0700

    testing toggle

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 $(function() {
-
 	setTimeout(function() {
 		$("body").removeClass("preload");
 	}, 400); 
+
+
+	$("#dropdown-toggle").click(function() {
+		if ($('#dropdown-toggle').attr('aria-expanded') == false) {
+			console.log('test');
+		}
+
+		// $('html, body').animate({
+		// 	scrollTop: $("#start").offset().top
+		// }, 500);
+	});
 });
