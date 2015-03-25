8a4efbe66b2f27bb52eddeb594805458f616a7db
Judit <hello@juditdidit.com>
2015-03-25 09:59:37 -0700

    created company index, updated to latest CSS, finalized energy index layout

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,21 @@
 $(function() {
+
+	// prevent css transition from triggering on page load
 	setTimeout(function() {
-		$("body").removeClass("preload");
+		$('body').removeClass('preload');
 	}, 400); 
 
-
-	$("#dropdown-toggle").click(function() {
+	//scroll "dropdown" box into view on index page
+	$('#dropdown-toggle').click(function() {
 		if ($('#dropdown-toggle').attr('aria-expanded') === 'false') {
 			$('html, body').animate({
-				scrollTop: $("#start").offset().top
+				scrollTop: $('#start').offset().top
 			}, 500);
 		}
 	});
 
-	$(".results-cards .card button").click(function(e) {
+	// results page card flip
+	$('.results-cards .card button').click(function(e) {
 		e.preventDefault();
 
 		var toggleButton = $(this);
@@ -20,4 +23,14 @@ $(function() {
 
 		toggleCard.toggleClass('flip');
 	});
+
+	// results page sticky table header
+	var cellProduct = $('#results tbody td.product').css('width');
+	var cellEScore = $('#results thead td.e-score').css('height');
+
+	console.log(cellProduct + ' ' + cellEScore);
+
+	function stickyTableHeader() {
+		
+	}
 });
