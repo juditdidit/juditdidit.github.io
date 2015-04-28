9a49b0c29df2a602cff2c1be2c17e1b65cbb584e
Judit <hello@juditdidit.com>
2015-04-28 09:08:38 -0700

    added grunt files, updated less files, added apple email sig file.

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      development: {
+        options: {
+        },
+        files: [{
+          expand: true,
+          cwd: 'enervee/assets/',
+          src: 'css-*.less',
+          ext: '.css',
+          dest: 'enervee/assets/'
+        },{
+          expand: true,
+          cwd: 'energizect/assets/',
+          src: 'css-*.less',
+          ext: '.css',
+          dest: 'energizect/assets/'
+        }]
+      }
+    },
+
+    watch: {
+      files: '**/css-*.less',
+      tasks: ['less']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('default', ['less']);
+};
