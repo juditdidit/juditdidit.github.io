40af147320de183ee4f32ba7888a69e8f67ae350
Judit <hello@juditdidit.com>
2023-05-23 13:19:29 -0700

    APP_BASE_HREF

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { APP_BASE_HREF } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
@@ -32,7 +33,9 @@ import { SiteNavComponent } from './shared/site-nav/site-nav.component';
         BrowserModule,
         AppRoutingModule
     ],
-    providers: [],
+    providers: [{
+        provide: APP_BASE_HREF, useValue: 'https://juditdidit.github.io',
+    }],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
